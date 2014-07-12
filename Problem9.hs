f55f7d591f73a64da71607a304f5f91943994044
Diullei Gomes <diullei@gmail.com>
2014-07-12 00:19:09 -0300

    refactoring problem 9

--- Problem9.hs
+++ Problem9.hs
@@ -2,7 +2,8 @@ import Test.QuickCheck
 import Test.QuickCheck.All
 
 {-
-    (**) Pack consecutive duplicates of list elements into sublists. If a list contains repeated elements they should be placed in separate sublists.
+    (**) Pack consecutive duplicates of list elements into sublists. If a list contains
+    repeated elements they should be placed in separate sublists.
 
     Example:
 
@@ -26,8 +27,22 @@ pack xs = do
     let (_, second) = splitAt (length result) xs
     [result] ++ (pack second)
 
+pack' :: Eq a => [a] -> [[a]]
+pack' [] = []
+pack' xs = do
+    let result = takeWhile (\x -> x == head xs) xs
+    [result] ++ (pack' (drop (length result) xs))
+
+pack'' :: Eq a => [a] -> [[a]]
+pack'' [] = []
+pack'' xs = [g] ++ (pack'' (drop (length g) xs))
+    where g = takeWhile (\x -> x == head xs) xs
+
 {- ------------- -}
 {- TEST CASE     -}
 {- ------------- -}
 
-main = quickCheck (pack ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e'] == ["aaaa","b","cc","aa","d","eeee"])
+main = do
+    quickCheck (pack ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e'] == ["aaaa","b","cc","aa","d","eeee"])
+    quickCheck (pack' ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e'] == ["aaaa","b","cc","aa","d","eeee"])
+    quickCheck (pack'' ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e'] == ["aaaa","b","cc","aa","d","eeee"])

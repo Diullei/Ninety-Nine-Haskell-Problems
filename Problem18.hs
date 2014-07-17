1494b0f1d9947e046ccaa15fe5087da32110f950
Diullei <diullei@gmail.com>
2014-07-17 01:18:17 -0300

    problem 18

---
+++ Problem18.hs
@@ -0,0 +1,42 @@
+import Test.QuickCheck
+import Test.QuickCheck.All
+
+{-
+    (**) Extract a slice from a list.
+
+    Given two indices, i and k, the slice is the list containing the elements between
+    the i'th and k'th element of the original list (both limits included). Start
+    counting the elements with 1.
+
+    Example:
+
+    * (slice '(a b c d e f g h i k) 3 7)
+    (C D E F G)
+    Example in Haskell:
+
+    *Main> slice ['a','b','c','d','e','f','g','h','i','k'] 3 7
+    "cdefg"
+-}
+
+{- ------------- -}
+{- SOLUTION HERE -}
+{- ------------- -}
+
+slice :: [a] -> Int -> Int -> [a]
+slice xs start end =
+    takeWhile' xs start end 1
+    where
+        takeWhile' :: [a] -> Int -> Int -> Int -> [a]
+        takeWhile' []     _     _   _     = []
+        takeWhile' (x:xs) start end index =
+            if index >= start && index <= end then
+                x:takeWhile' xs start end (index + 1)
+            else
+                takeWhile' xs start end (index + 1)
+
+
+{- ------------- -}
+{- TEST CASE     -}
+{- ------------- -}
+
+main = quickCheck (slice ['a','b','c','d','e','f','g','h','i','k'] 3 7 == "cdefg")
